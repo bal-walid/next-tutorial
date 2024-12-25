0b12a9b6e289cb2ac971f8f15f235b2f9202cf43
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-25 11:55:24 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter, Lusitana} from 'next/font/google';
+import { Playfair_Display_SC } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin'], weight: "400"});
+export const lusitana = Lusitana({subsets: ['latin'], weight: ["400", "700"]});
